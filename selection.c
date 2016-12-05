d2bd00cdedd73389ebd41e0cabbd359629365bf2
Lovelace973 <andydinga@hotmail.fr>
2016-12-05 11:18:32 +0100

    ajout fct selection.c

---
+++ selection.c
@@ -0,0 +1,14 @@
+#include "Outil.h"
+#define N 15
+
+void selection(char grille[N][N], int x,int y){
+	int i,j;	
+	printf("Coordonnées de début de mot :\n");
+	scanf("%i",&x);
+	printf("Coordonnées de début de mot :\n");
+	scanf("%i",&y);
+}
+
+void verification(char grille[N][N], int x, int y){
+	
+	
